436c611bbabfba0c20ec25d2fed38fb41ff62cd2
Fredrik Tidemann <fredrik.tid@gmail.com>
2022-07-03 20:14:12 +0200

    Updates button with script setup

--- src/components/ConvertButton.vue
+++ src/components/ConvertButton.vue
@@ -1,25 +1,22 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  disabled: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+</script>
+
 <template>
   <button
     :disabled="disabled"
     class="disabled:opacity-50 disabled:cursor-not-allowed bg-gray-800 transition text-white py-3 px-6 rounded hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none"
-  >
-    {{ text }}
-  </button>
+    v-text="text"
+  />
 </template>
-
-<script>
-export default {
-  props: {
-    text: {
-      type: String,
-      default: 'Button',
-      required: true
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-      required: false
-    }
-  }
-}
-</script>
